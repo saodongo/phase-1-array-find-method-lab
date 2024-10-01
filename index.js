35dbbac698a4963391bd0510842bcb3f6d8c0e1d
sharonodongo <sharon94odongo@gmail.com>
2024-10-01 22:33:06 +0300

    submitting assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(game => game.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
